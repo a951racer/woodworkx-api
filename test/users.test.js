046772512946990b77836976acf77a0fe42e1622
Jon Hobbs <jon.hobbs@minnykid.com>
2024-08-28 23:15:55 -0500

    Converted test files to Mocha and Chai

--- test/users.test.js
+++ test/users.test.js
@@ -1,11 +1,12 @@
 import request from 'supertest'
-import app from '../index.js'
+import app from '../src/index.js'
+import { expect } from 'chai'
 
 let token
 let userId
 
-describe('User Endpoints', () => {
-    it('should register test user', async () => {
+describe('User Endpoints', function() {
+    it('should register test user', async function() {
         const res = await request(app)
             .post('/auth/register')
             .send({
@@ -15,33 +16,33 @@ describe('User Endpoints', () => {
                 lastName: 'Doe'
             })
             .expect(200)
-        expect(res.body.firstName).toEqual('John')
-        expect(res.body.lastName).toEqual('Doe')
+        expect(res.body.firstName).to.equal('John')
+        expect(res.body.lastName).to.equal('Doe')
         userId = res.body._id
     })
 
-    it('should log user in', async () => {
+    it('should log user in', async function() {
         const res = await request(app)
             .post('/auth/login')
             .send({
                 email: 'joe.blow@test.com',
-                password: 'password123',
+                password: 'Jeg22713',
             })
            .expect(200)
         token = res.body.token
     })
 
-    it('should fetch user', async () => {
+    it('should fetch user', async function() {
         const res = await request(app)
             .get(`/user/${userId}`)
             .set('Authorization', `Bearer ${token}`)
             .expect(200)
-        expect(res.body.firstName).toEqual('John')
-        expect(res.body.lastName).toEqual('Doe')
-        expect(res.body.email).toEqual('john.doe@test.com')
+        expect(res.body.firstName).to.equal('John')
+        expect(res.body.lastName).to.equal('Doe')
+        expect(res.body.email).to.equal('john.doe@test.com')
     })
 
-    it('should update user', async () => {
+    it('should update user', async function() {
         const res = await request(app)
             .put(`/user/${userId}`)
             .send({
@@ -51,16 +52,16 @@ describe('User Endpoints', () => {
             })
             .set('Authorization', `Bearer ${token}`)
             .expect(200)
-        expect(res.body.firstName).toEqual('Jimmy')
-        expect(res.body.lastName).toEqual('Hooligan')
-        expect(res.body.email).toEqual('jimmy.hooligan@tester.com')
+        expect(res.body.firstName).to.equal('Jimmy')
+        expect(res.body.lastName).to.equal('Hooligan')
+        expect(res.body.email).to.equal('jimmy.hooligan@tester.com')
     })
 
-    it('should delete user', async () => {
+    it('should delete user', async function() {
         const res = await request(app)
             .delete(`/user/${userId}`)
             .set('Authorization', `Bearer ${token}`)
             .expect(200)
-        expect(res.body.message).toEqual('Successfully deleted User')
+        expect(res.body.message).to.equal('Successfully deleted User')
     })
 })

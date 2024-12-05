f296576e197390a730287d1550167be6823520bd
Jon Hobbs <jon.hobbs@minnykid.com>
2024-12-05 12:29:58 -0600

    removed some logging

--- src/files/fileController.js
+++ src/files/fileController.js
@@ -34,7 +34,6 @@ export default {
         const { body } = req
         const { mediaType, id } = body
         const file = req.file
-        console.log('req: ', req)
         let folder = ''
         let fileName = ''
 
